24cea1c6592a668e2c1aae56d1c2cb97c475dc58
Clyde Broderick <clyderick3@gmail.com>
2022-02-28 17:14:20 -0500

    Did even and odds.

--- src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -3,12 +3,28 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String result = "";
+        for (int i = start; i < stop; i++) {
+            if (i % 2 == 0) {
+                result += i; // result = result + i
+            }
+            //return result;
+        }
+        return result;
     }
 
 
+
+
+
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String result = "";
+        for (int i = start; i<stop; i++) {
+            if (i % 2 != 0) {
+                result += i;
+            }
+        }
+        return result;
     }
 
 
