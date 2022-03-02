6e1efca6f5bca11b88b1c7f2d3c84e9ce51b7c28
Clyde Broderick <clyderick3@gmail.com>
2022-03-02 13:39:37 -0500

    Completed the multiplication tables.

---
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/GMT.java
@@ -0,0 +1,12 @@
+package io.zipcoder.microlabs.mastering_loops;
+
+public class GMT {
+    public static void main(String[] args) {
+
+        int num = 5;
+        for(int i = 1; i <= 10; ++i)
+        {
+            System.out.printf("%d * %d = %d \n", num, i, num * i);
+        }
+    }
+}
--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -1,22 +1,31 @@
 package io.zipcoder.microlabs.mastering_loops;
 
+//import jdk.javadoc.internal.doclets.formats.html.Table;
+
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        return TableUtilities.getMultiplicationTable(4);
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        return TableUtilities.getMultiplicationTable(9);
     }
 
     public static String getMultiplicationTable(int tableSize) {
         String result = "";
+        StringBuilder gmt = new StringBuilder();
+
         for (int i = 1; i <= tableSize; i++) {
+           // System.out.println(i * tableSize);
             for (int j = 1; j <= tableSize; j++) {
-               System.out.print(i * j);
-result = String.format(%4d,)
+                //System.out.print(i*j * tableSize);
+                gmt.append(String.format("%3d |", i*j));
+
             }
+            gmt.append("\n");
         }
+        result = gmt.toString();
+
         return result;
 
     }
